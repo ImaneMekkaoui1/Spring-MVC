acf5297e2034c8263470b00c3e23359ecaae414e
ImaneMekkaoui <mekkaouiimane722@gmail.com>
2026-03-08 06:51:35 +0000

    Ajout du layout Thymeleaf, formulaire de saisie et validation des produits

--- src/main/java/org/example/springmvc2/web/ProductController.java
+++ src/main/java/org/example/springmvc2/web/ProductController.java
@@ -1,13 +1,16 @@
 package org.example.springmvc2.web;
 
 
+import jakarta.validation.Valid;
 import org.example.springmvc2.entities.Product;
 import org.example.springmvc2.repository.ProductRepository;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -26,11 +29,33 @@ public class ProductController {
 
        return "products";
     }
+
+    @GetMapping("/")
+    public String home() {
+
+        return "redirect:/index";
+    }
+
     @GetMapping("/delete")
     public String delete(@RequestParam(name="id") Long id) {
         productRepository.deleteById(id);
         return "redirect:/index";
 
+    }
+
+    @GetMapping("/newProduct")
+    public String newProduct(Model model) {
+        model.addAttribute("product", new Product());
+        return "new-product";
+
+    }
+
+    @PostMapping("/saveProduct")
+    public String saveProduct(@Valid Product product, BindingResult bindingResult , Model model) {
+        if (bindingResult.hasErrors())
+            return "new-product";
+        productRepository.save(product);
+        return "redirect:/index";
     }
 
 }
