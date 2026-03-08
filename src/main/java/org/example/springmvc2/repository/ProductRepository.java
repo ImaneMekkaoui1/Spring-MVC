b1d1a5d292cc82de89e224da59d6ecf67af31f4c
ImaneMekkaoui <mekkaouiimane722@gmail.com>
2026-03-08 17:17:11 +0000

    last commit + some update

--- src/main/java/org/example/springmvc2/repository/ProductRepository.java
+++ src/main/java/org/example/springmvc2/repository/ProductRepository.java
@@ -2,9 +2,12 @@ package org.example.springmvc2.repository;
 import org.example.springmvc2.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * @author Imane Mekkaoui
  */
 public interface ProductRepository extends JpaRepository<Product ,Long> {
+    List<Product> findByNameContains(String keyword);
 
 }
